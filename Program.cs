0a6f36d30f313766e554fd052c7ed4fa469557ab
nazliozbek <102694546+nazliozbek@users.noreply.github.com>
2022-04-02 13:34:06 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace dongu_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while
+            ////1 den baslayarak consoldan girilen sayilarin ortalamasini hesaplayip yazdiran program
+            /*Console.WriteLine("lutfen bir sayi giriniz:");
+            int sayi=int.Parse(Console.ReadLine());
+            int sayac=1;
+            int toplam=0;
+            while(sayac<=sayi)
+            {
+                toplam+=sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam/sayi);
+
+            //a'dan z'ye kadar tum harfleri konsola yazdiran program
+            char character='a';
+            while(character<'z')
+            {
+                Console.Write(character);
+                character++;
+            }*/
+
+            Console.WriteLine(".....FOREACH.....");
+            string[] arabalar={"BMW","Ford","Toyota","Nissan"};
+            
+            foreach(var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        
+        }
+        
+
+
+    }
+}
